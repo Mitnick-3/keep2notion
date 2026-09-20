5f6a2916d72f027f3dd7fbfcd01e4456b1db0b1e
Mitnick-3 <53416267+Mitnick-3@users.noreply.github.com>
2026-09-20 14:19:10 +0800

    Update notion_helper.py

--- keep2notion/notion_helper.py
+++ keep2notion/notion_helper.py
@@ -251,8 +251,7 @@ class NotionHelper:
         has_more = True
         start_cursor = None
         while has_more:
-            #response = self.client.databases.query(
-            responese= self.client.databases.list_items(
+            responese = self.client.databases.list_items(
                 database_id=database_id,
                 start_cursor=start_cursor,
                 page_size=100,
